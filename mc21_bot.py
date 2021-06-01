edabe6d6166dbaa4ade37ec874fb0e1e1c786cce
roctbb <roctbb@gmail.com>
2021-06-01 19:26:34 +0300

    start develop

--- mc21_bot.py
+++ mc21_bot.py
@@ -26,7 +26,20 @@ def status(data):
 @app.route('/order', methods=['POST'])
 @verify_json
 def order(data):
-    pass
+    print(data)
+    contract_id = int(data.get('contract_id'))
+    info = medsenger_api.get_patient_info(contract_id)
+
+    if info or not info.get('phone') or not data.get('params', {}).get('message'):
+        alert = Alert(contract_id=contract_id, name=info.get('name'), birthday=info.get('birthday'),
+                             phone=info.get('phone'), message=data.get('params').get('message'))
+        db.session.add(alert)
+        db.session.commit()
+
+        medsenger_api.send_message(is_urgent=True, contract_id=contract_id, only_patient=True, text="В течение нескольких минут с Вами свяжется врач контакт-центра МЦ 21 век по телефону {}.".format(info.get('phone')))
+        medsenger_api.send_message(is_urgent=True, need_answer=True, contract_id=contract_id, only_doctor=True, text="Отправлен запрос в контакт центр для экстренной связи с пациентом.")
+    else:
+        abort(422)
 
 
 # contract management api

033acbf91c3be094544e2df72bce099ced0602a0
roctbb <roctbb@gmail.com>
2021-08-31 13:33:27 +0300

    add integration with custom params

--- mc21_bot.py
+++ mc21_bot.py
@@ -74,12 +74,15 @@ def init(data):
         "emergency_info": "Информация на случай экстренной ситуации"
     }
 
+    print(info)
+
     for search_param in ['address', 'card_number', 'emergency_info']:
         param_name = param_names[search_param]
 
         if params.get(search_param):
             setattr(contract, param_name, params.get(search_param))
         elif info.get('additional_params', {}).get(search_param):
+            print(param_name, info.get('additional_params', {}).get(additional_param_names[search_param]))
             setattr(contract, param_name, info.get('additional_params', {}).get(additional_param_names[search_param]))
 
     medsenger_api.send_message(contract_id=data.get('contract_id'), only_doctor=True, action_link='settings',
